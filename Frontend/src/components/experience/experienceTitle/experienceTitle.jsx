93b43da81ee62133b5ad2f59336f3e872b243cd7
MelvinYG <melvingeorge204@gmail.com>
2024-09-05 10:25:43 +0530

    fix: Experience Title responsive

--- Frontend/src/components/experience/experienceTitle/experienceTitle.jsx
+++ Frontend/src/components/experience/experienceTitle/experienceTitle.jsx
@@ -5,9 +5,14 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const ExperienceTitle = () => {
+    let windowWidth = window.innerWidth;
     useGSAP(() => {
+        let titleWidth = document.querySelector('.exp-title h1').scrollWidth;
+        titleWidth = Math.min(0.9*titleWidth, 2000);
+        console.log(titleWidth);
+        let transX = -1*titleWidth;
         gsap.to('.exp-title h1', {
-            x: "-90%",
+            x: transX,
             scrollTrigger: {
                 trigger: ".exp-title",
                 start: "top 10%",
@@ -15,12 +20,12 @@ const ExperienceTitle = () => {
                 scrub: 2,
                 pin: true,
             }
-        });
+        },[windowWidth]);
     });
 
     return (
-        <div className="exp-title uppercase h-[600px] bg-blue-300">
-            <h1 className="text-[450px] px-20">Experience</h1>
+        <div className="exp-title uppercase h-[calc(70vh)] bg-blue-300">
+            <h1 className="text-[calc(50vh)] px-20">Experience</h1>
         </div>
     );
 };
